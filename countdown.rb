2169aa785fa6f80344483ec551dec1641a484b1b
Yonnick Brown <25hour24@gmail.com>
2019-01-27 11:07:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,15 @@ def countdown(number)
   while counter > 0
     puts "#{counter} SECOND(S)!"
     counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+  counter = number
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
     sleep(1)
   end
   "HAPPY NEW YEAR!"
